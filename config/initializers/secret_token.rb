a3cd04c6e53c1a2f895ba62fa64248a41dcbd82c
Monica Olinescu <monica.olinescu@gmail.com>
2013-01-13 18:43:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timezonia::Application.config.secret_token = '7c367d26d69eaf327d1e3440ab02d655f0207adb4a47f25de619755e412938fa05b403c9f99f4212fe06ab7b74b5646a19b0eedfb8ec88f6e274c39ebd165aac'
